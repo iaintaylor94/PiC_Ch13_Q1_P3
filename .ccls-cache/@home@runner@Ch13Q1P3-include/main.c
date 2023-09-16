e4d01c26f6c264957645eb59321abce1381e937a
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-16 13:50:36 +0000

    copied program

--- .ccls-cache/@home@runner@Ch13Q1P3-include/main.c
+++ .ccls-cache/@home@runner@Ch13Q1P3-include/main.c
@@ -1,6 +1,19 @@
+/* Program to illustrate the use of the #include statement
+   Note: This program assumes that definitions are set up in a file called metric.h */
+
 #include <stdio.h>
+#include "metric.h"
 
 int main(void) {
-  printf("Hello World\n");
+
+  float liters, gallons;
+
+  printf ("***Liters to gallons***\n\n");
+  printf ("Enter the number of liters: ");
+  scanf ("%f", &liters);
+
+  gallons = liters * QUARTS_PER_LITER / 4.0;
+  printf ("%g liters = %g gallons\n", liters, gallons);
+  
   return 0;
 }
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Programming in C
+## Chapter 13
+### Question 1 : Part 3
+
+This problem is from chapter 13 of the textbook[^1]. This program[^2] demonstrates the use of the #include statement.
+
+You may wish to run the program and compare its output.
+
+
+[^1]: Programming in C (Third edition) By Stephen G. Kochan - Page 320 : Question 1
+[^2]: Programming in C (Third edition) By Stephen G. Kochan - Page 314 : Problem 1.3
